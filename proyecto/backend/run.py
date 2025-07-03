a2d5b654afc43d78ced0ee01d9753c4b1cbde872
Lucrion <jhan.servantes.b@gmail.com>
2025-07-03 14:04:40 -0400

    Delete proyecto directory

--- proyecto/backend/run.py
+++
@@ -1,5 +0,0 @@
-# Este archivo sirve como punto de entrada alternativo para ejecutar la aplicación
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
